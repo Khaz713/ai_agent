57c118e7c36c4acd43f12de86ea74006fd57e5ca
Khaz713 <konrad713@gmail.com>
2025-07-21 11:30:04 +0200

    added asking questions and loging

---
+++ log.py
@@ -0,0 +1,33 @@
+from google import genai
+import json
+from datetime import datetime
+import os
+
+def get_date_key():
+    now = datetime.now()
+    return f"{now.year}-{now.month:02d}"
+
+def get_log():
+    with open('log.json', 'r', encoding='utf-8') as f:
+        if os.path.getsize('log.json') == 0:
+            return {get_date_key(): []}
+        return json.load(f)
+
+
+def append_log(prompt, response, response_time):
+    log = get_log()
+    with open('log.json', 'w', encoding='utf-8') as f:
+        log[get_date_key()].append({
+            'prompt': prompt,
+            'response': response.text,
+            'prompt_tokens': response.usage_metadata.prompt_token_count,
+            'response_tokens': response.usage_metadata.candidates_token_count,
+            'response_time': response_time
+        })
+        json.dump(log, f, indent=4, ensure_ascii=False)
+
+def get_used_prompt_tokens(log):
+    used_prompt_tokens = 0
+    for prompt in log[get_date_key()]:
+        used_prompt_tokens += prompt['prompt_tokens']
+    return used_prompt_tokens
--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+import time
+
+from log import append_log, get_used_prompt_tokens, get_log
+
+
 def main():
-    print("Hello from ai-agent!")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <prompt>")
+        sys.exit(1)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    start = time.time()
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=sys.argv[1])
+    end = time.time()
+    response_time = end - start
+    append_log(sys.argv[1], response, response_time)
+    tokens_used = get_used_prompt_tokens(get_log())
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+          f"Response tokens: {response.usage_metadata.candidates_token_count}\n"
+          f"Tokens used this month: {tokens_used}\n"
+          f"Response:\n {response.text}")
 
 
 if __name__ == "__main__":
